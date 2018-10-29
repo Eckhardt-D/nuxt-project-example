5ac52bffe44db337a9b2880555c7d0b5addd00dc
ecki-d <support@kaizenmedia.co.za>
2018-10-29 09:55:01 +0200

    Adapt email templates to fix render issues.

--- store/modules/journey.js
+++ store/modules/journey.js
@@ -25,7 +25,7 @@ const actions = {
     Vue.prototype.$vista.journey
     .sendTemplateMail({
       from: 'rainmaker VISTA.journey <journey@rainmaker.travel>',
-      to: ['"Eckhardt Dreyer" <eckhardt@rainmaker.travel>'],
+      to: ['"Eckhardt Dreyer" <eckhardt@rainmaker.travel>', '"Stephen Lombard" <stephen@rainmaker.travel>', '"Thomas Muller" <thomas@rainmaker.travel>'],
       replyTo: payload.email,
       subject: `New WebSite Enquiry | ${payload.department}`,
       templateId: 'feebb6da-0eb6-47ed-b7e0-10764c3aa256',
