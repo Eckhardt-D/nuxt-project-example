284eac87d332286154ff1d79dd3556ad91eb974b
ecki-d <support@kaizenmedia.co.za>
2018-11-05 15:00:00 +0200

    contact pages

--- pages/contact.vue
+++ pages/contact.vue
@@ -16,16 +16,17 @@
     <div class="inner-offset darken-block">
       <div class="container-fluid">
         <ul class="nav nav-tabs wow zoomIn" data-wow-delay="0.3s" id="filter">
-          <li @click="setSelectedPlace" id="activeItem" class="active"><a href="#" data-filter=".windhoek">Windhoek</a></li>
-          <li @click="setSelectedPlace"><a href="#" data-filter=".walvisbay">Walvis Bay</a></li>
-          <li @click="setSelectedPlace"><a href="#" data-filter=".karibib">Karibib</a></li>
-          <li @click="setSelectedPlace"><a href="#" data-filter=".mariental">Mariental</a></li>
-          <li @click="setSelectedPlace"><a data-filter=".keetmans" href="#" >Keetmanshoop</a></li>
-          <li @click="setSelectedPlace"><a href="#" data-filter=".joburg">Johannesburg</a></li>
           <li @click="setSelectedPlace"><a href="#" data-filter=".capetown">Cape Town</a></li>
+          <li @click="setSelectedPlace"><a href="#" data-filter=".durban">Durban</a></li>
+          <li @click="setSelectedPlace"><a href="#" data-filter=".joburg">Johannesburg</a></li>
+          <li @click="setSelectedPlace"><a href="#" data-filter=".karibib">Karibib</a></li>
+          <li @click="setSelectedPlace"><a data-filter=".keetmans" href="#" >Keetmanshoop</a></li>
+          <br>
+          <li @click="setSelectedPlace"><a href="#" data-filter=".mariental">Mariental</a></li>
           <li @click="setSelectedPlace"><a href="#" data-filter=".oshakati">Oshakati</a></li>
           <li @click="setSelectedPlace"><a href="#" data-filter=".tsumeb">Tsumeb</a></li>
-          <li @click="setSelectedPlace"><a href="#" data-filter=".durban">Durban</a></li>
+          <li @click="setSelectedPlace"><a href="#" data-filter=".walvisbay">Walvis Bay</a></li>
+          <li @click="setSelectedPlace" id="activeItem" class="active"><a href="#" data-filter=".windhoek">Windhoek</a></li>
         </ul>
       </div>
     </div>
